1433a222ae76a4ebba6905fb87110d96b4f17567
Rob Glass <rglass@getwellnetwork.com>
2017-08-06 16:55:51 -0400

    more functional tests. Commiting proir to refactor

--- functional_test.py
+++ functional_test.py
@@ -34,17 +34,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_table_list')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Finish App' for row in rows),
-            "New to-do item did not appear in table"
-        )
+
+        self.assertIn('1: Finish App', [row.text for row in rows])
 
         # There is still a textbox for the User to enter another item. User enters "Upload to git."
-        self.fail('Finish the tests!')
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        inputBox.send_keys('Push app to git.')
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Page updates again, and now shows both items
+        table = self.browser.find_element_by_id('id_table_list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Finish App', [row.text for row in rows])
+        self.assertIn('2: Push app to git.', [row.text for row in rows])
+
 
         # User notices sites URL has changed.
 
         # navigating to new URL shows same to-do list.
+        self.fail('Finish the tests!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
