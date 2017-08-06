626928a860a179f271adbd3bc0fc63195d3d2e00
Rob Glass <rglass@getwellnetwork.com>
2017-08-06 15:40:20 -0400

    Refactor home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,15 +15,31 @@ class NewVisitorTest(unittest.TestCase):
         # User notes the page title and header mention to-do lists.
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is invited to enter new To-do item right away
+        inputBox =self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # User types "Finish App" in to a text box
+        inputBox.send_keys('Finish App')
 
         # When User hits enter, the page updates, and now the page lists "1. Finish App" in a to-do list
+        inputBox.send_keys(Keys.Enter)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_table_list')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Finish App' for row in rows)
+        )
 
         # There is still a textbox for the User to enter another item. User enters "Upload to git."
+        self.fail('Finish the tests!')
 
         # User notices sites URL has changed.
 
